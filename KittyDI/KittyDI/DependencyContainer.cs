4f0320992fc0b0f4e2364a6e37112d0396247809
Markus Palcer <Markus.Palcer@GMail.com>
2017-06-09 01:59:07 +0200

    Initial commit

---
+++ KittyDI/KittyDI/DependencyContainer.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KittyDI
+{
+  [AttributeUsage(AttributeTargets.Constructor)]
+  public class ProvidingConstructorAttribute : Attribute
+  {
+    
+  }
+
+  public interface IDependencyContainer
+  {
+    T Resolve<T>();
+  }
+
+  public class DependencyException : Exception { }
+
+  public class NoSuitableConstructorFoundException : DependencyException
+  {
+    public NoSuitableConstructorFoundException(Type targetType)
+    {
+      TargetType = targetType;
+    }
+
+    public Type TargetType { get; set; }
+  }
+
+  public class DependencyContainer : IDependencyContainer
+  {
+    private readonly Dictionary<Type, Func<object>> _factories = new Dictionary<Type, Func<object>>();
+
+    public T Resolve<T>()
+    {
+      return ResolveFactory<T>()();
+    }
+
+    public Func<T> ResolveFactory<T>()
+    {
+      var factory = ResolveFactory(typeof(T));
+
+      return () => (T) factory();
+    }
+
+    private Func<object> ResolveFactory(Type T)
+    {
+      Func<object> factory;
+      if (_factories.TryGetValue(T, out factory))
+      {
+        return factory;
+      }
+
+      factory = CreateFactory(T);
+      _factories[T] = factory;
+      return factory;
+    }
+
+    private Func<object> CreateFactory(Type resultType)
+    {
+      var constructors = resultType.GetConstructors();
+
+      var constructor = constructors.FirstOrDefault(x => x.GetParameters().Length == 0);
+      if (constructor != null)
+      {
+        return () => constructor.Invoke(new object[] {});
+      }
+
+      if (constructors.Length == 1)
+      {
+        constructor = constructors.First();
+      }
+      else
+      {
+        constructor = constructors.SingleOrDefault(x => x.GetCustomAttribute<ProvidingConstructorAttribute>() != null);
+      }
+      
+      if (constructor != null)
+      {
+        var parameterFactories = constructor.GetParameters().Select(param => ResolveFactory(param.ParameterType)).ToArray();
+        return () => constructor.Invoke(parameterFactories.Select(x => x()).ToArray());
+      }
+
+      throw new NoSuitableConstructorFoundException(resultType);
+    }
+
+    public void RegisterFactory<T>(Func<T> factory)
+    {
+      RegisterFactory<T, T>(factory);
+    }
+
+    public void RegisterFactory<TContract, TImplementation>(Func<TImplementation> factory)
+    where TImplementation : TContract
+    {
+      _factories.Add(typeof(TContract), () => factory());
+    }
+
+    public void RegisterInstance<T>(T instance)
+    {
+      RegisterInstance<T, T>(instance);
+    }
+
+    /// <summary>
+    /// Registers an instance of an object as singleton
+    /// </summary>
+    /// <param name="instance"></param>
+    public void RegisterInstance<TContract, TImplementation>(TImplementation instance)
+    where TImplementation : TContract
+    {
+      RegisterFactory<TContract, TImplementation>(() => instance);
+    }
+
+    public void RegisterImplementation<TContract, TImplementation>()
+      where TImplementation : TContract
+    {
+      _factories.Add(typeof(TContract), ResolveFactory(typeof(TImplementation)));
+    }
+  }
+}
---
+++ KittyDI/TestKitten/DependencyContainerTests.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using FluentAssertions;
+using KittyDI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace TestKitten
+{
+
+  public interface ITestInterface { }
+
+  public class TestImplementation : ITestInterface
+  {
+  }
+
+  public class TypeWithUnsuitableConstructor
+  {
+    public TypeWithUnsuitableConstructor(int d) { }
+
+    public TypeWithUnsuitableConstructor(int d, int d2) { }
+  }
+
+  public class TypeWithSingleConstructor
+  {
+    public TypeWithSingleConstructor(int d) { }
+  }
+
+  public class NestedResolutionType : ITestInterface
+  {
+    public NestedResolutionType(TypeWithSingleConstructor t) { }
+  }
+
+  public class MarkedConstructorType
+  {
+    [ProvidingConstructor]
+    public MarkedConstructorType(int d) { }
+
+    public MarkedConstructorType(ITestInterface t)
+    {
+      throw new NotImplementedException();
+    }
+  }
+
+  [TestClass]
+  public class DependencyContainerTests
+  {
+    [TestMethod]
+    public void ManuallyProvidingFactory()
+    {
+      var calls = 0;
+      var mock = new TestImplementation();
+      var sut = new DependencyContainer();
+      sut.RegisterFactory(() =>
+      {
+        calls++;
+        return mock;
+      });
+
+      sut.ResolveFactory<TestImplementation>()().Should().Be(mock);
+      sut.Resolve<TestImplementation>().Should().Be(mock);
+      calls.Should().Be(2);
+    }
+
+    [TestMethod]
+    public void ManuallyProvidingFactoryForInterface()
+    {
+      var calls = 0;
+      var mock = new TestImplementation();
+      var sut = new DependencyContainer();
+      sut.RegisterFactory<ITestInterface, TestImplementation>(() =>
+      {
+        calls++;
+        return mock;
+      });
+
+      sut.ResolveFactory<ITestInterface>()().Should().Be(mock);
+      sut.Resolve<ITestInterface>().Should().Be(mock);
+      calls.Should().Be(2);
+    }
+
+    [TestMethod]
+    public void ManuallyProvidingInstance()
+    {
+      var mock = new TestImplementation();
+      var sut = new DependencyContainer();
+      sut.RegisterInstance(mock);
+
+      sut.ResolveFactory<TestImplementation>()().Should().Be(mock);
+      sut.Resolve<TestImplementation>().Should().Be(mock);
+    }
+
+    [TestMethod]
+    public void ManuallyProvidingInstanceForInterface()
+    {
+      var mock = new TestImplementation();
+      var sut = new DependencyContainer();
+      sut.RegisterInstance<ITestInterface, TestImplementation>(mock);
+
+      sut.ResolveFactory<ITestInterface>()().Should().Be(mock);
+      sut.Resolve<ITestInterface>().Should().Be(mock);
+    }
+
+    [TestMethod]
+    public void AutoFactoryCreationForParameterlessConstructors()
+    {
+      var sut = new DependencyContainer();
+      sut.Resolve<TestImplementation>().Should().BeOfType<TestImplementation>();
+    }
+
+    [TestMethod]
+    public void TypeRedirection()
+    {
+      var sut = new DependencyContainer();
+      sut.RegisterImplementation<ITestInterface, TestImplementation>();
+      sut.Resolve<TestImplementation>().Should().BeOfType<TestImplementation>();
+    }
+
+    [TestMethod]
+    public void NoSuitableConstructorPresent()
+    {
+      var sut = new DependencyContainer(); 
+      sut.Invoking(x => x.Resolve<TypeWithUnsuitableConstructor>())
+        .ShouldThrow<NoSuitableConstructorFoundException>();
+    }
+
+    [TestMethod]
+    public void AutoResolvingOfSingleConstructorParameters()
+    {
+      var sut = new DependencyContainer();
+      sut.RegisterInstance(2);
+      sut.Invoking(x => x.Resolve<TypeWithSingleConstructor>())
+        .ShouldNotThrow(); 
+    }
+
+    [TestMethod]
+    public void NestedRedirectedResolution()
+    {
+      var sut = new DependencyContainer();
+      sut.RegisterInstance(2);
+      sut.RegisterImplementation<ITestInterface, NestedResolutionType>();
+      sut.Resolve<ITestInterface>().Should().BeOfType<NestedResolutionType>();
+    }
+
+    [TestMethod]
+    public void MultipleConstructorsNeedAttribute()
+    {
+      var sut = new DependencyContainer(); 
+      sut.RegisterInstance(2);
+      sut.Invoking(x => x.Resolve<MarkedConstructorType>())
+        .ShouldNotThrow();
+    }
+  }
+}
